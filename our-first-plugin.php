3ddd1cedfc031e0a82236ffba33d595d8a7e4495
wpseemol <wpseemol@gmail.com>
2025-08-07 20:22:07 +0600

    out fir commit start here

---
+++ our-first-plugin.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Plugin Name: Our First Plugin
+ * Plugin URI: https://example.com
+ * Author: Seemol Chakroborit
+ * Author URI: #
+ * Description: Dscripton here.
+ * Verstion: 1.0.0
+ * License: GPLv2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html 
+ * 
+ */
